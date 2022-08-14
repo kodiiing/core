068d262e57a49f3192cbdd86fff4c74c2b2dc8d4
Reinaldy Rafli <aldy505@proton.me>
2022-08-14 20:53:16 +0700

    feat: authenticate interface (#2)

--- auth/auth.go
+++ auth/auth.go
@@ -1,9 +1,42 @@
 // auth provides authentication procedure
 package auth
 
+import (
+	"context"
+	"net/url"
+	"time"
+)
+
 type Provider uint8
 
 const (
 	ProviderGithub Provider = iota
 	ProviderGitlab
 )
+
+type User struct {
+	ID       int64
+	Provider Provider
+	NodeID   string
+	// Name refers to the display name of the user
+	// that is displayed on their corresponding profile page.
+	Name             string
+	Username         string
+	AvatarURL        *url.URL
+	ProfileURL       *url.URL
+	Location         string
+	Email            string
+	PublicRepository int64
+	Followers        int64
+	Following        int64
+	// CreatedAt referes to the time that the user
+	// register to their corresponding provider.
+	CreatedAt time.Time
+	// RegisteredAt refers to the time that the user is register
+	// to the Kodiiing platform
+	RegisteredAt time.Time
+}
+
+type Authenticate interface {
+	Authenticate(ctx context.Context, accessToken string) (*User, error)
+}
